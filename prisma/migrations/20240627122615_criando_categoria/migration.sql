dbc31fe7e80a4cf84295c5d4cafd981c3050424c
Anthony <anthony.silva@avmb.com.br>
2024-06-27 09:27:08 -0300

    refactor - migration categoria

--- prisma/migrations/20240627122615_criando_categoria/migration.sql
+++ prisma/migrations/20240627122615_criando_categoria/migration.sql
@@ -8,9 +8,21 @@ CREATE TABLE "Abrigo" (
 );
 
 -- CreateTable
-CREATE TABLE "Usuario" (
+CREATE TABLE "Item" (
     "id" SERIAL NOT NULL,
     "nome" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "abrigoId" INTEGER NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nomeUsuario" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "abrigoId" INTEGER NOT NULL,
 
@@ -18,34 +30,20 @@ CREATE TABLE "Usuario" (
 );
 
 -- CreateTable
-CREATE TABLE "Item" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "descricao" TEXT,
-    "quantidade" INTEGER NOT NULL,
-    "abrigoId" INTEGER NOT NULL,
-
-    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Doacao" (
     "id" SERIAL NOT NULL,
-    "itemId" INTEGER NOT NULL,
     "quantidade" INTEGER NOT NULL,
     "data" TIMESTAMP(3) NOT NULL,
+    "itemId" INTEGER NOT NULL,
 
     CONSTRAINT "Doacao_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_abrigoId_fkey" FOREIGN KEY ("abrigoId") REFERENCES "Abrigo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Usuario" ADD CONSTRAINT "Usuario_abrigoId_fkey" FOREIGN KEY ("abrigoId") REFERENCES "Abrigo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Item" ADD CONSTRAINT "Item_abrigoId_fkey" FOREIGN KEY ("abrigoId") REFERENCES "Abrigo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Doacao" ADD CONSTRAINT "Doacao_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
