c0abe1910ea35ca0e061b8957d7cd08a5d9b98bd
Anthony <anthony.silva@avmb.com.br>
2024-06-26 22:41:38 -0300

    refactor - ant

--- src/server.ts
+++ src/server.ts
@@ -107,15 +107,15 @@ app.get('/abrigos/:id', async (request, reply) => {
 app.post('/abrigos', async (request, reply) => {
   const createAbrigoSchema = z.object({
     nome: z.string(),
-    localizacao: z.string(),
+    endereco: z.string(),
   });
 
-  const { nome, localizacao } = createAbrigoSchema.parse(request.body);
+  const { nome, endereco } = createAbrigoSchema.parse(request.body);
 
   await prisma.abrigo.create({
     data: {
       nome,
-      localizacao,
+      endereco,
     },
   });
 
@@ -126,7 +126,7 @@ app.put('/abrigos/:id', async (request, reply) => {
   const { id } = request.params as { id: string };
   const updateAbrigoSchema = z.object({
     nome: z.string().optional(),
-    localizacao: z.string().optional(),
+    endereco: z.string().optional(),
   });
 
   const data = updateAbrigoSchema.parse(request.body);
